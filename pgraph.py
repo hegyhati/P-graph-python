028e94e08de1947774a03b4417c5cdd758cadcb2
Mate Hegyhati <hegyhati.work@gmail.com>
2022-10-08 19:00:25 +0200

    display_name

--- pgraph.py
+++ pgraph.py
@@ -5,13 +5,13 @@ from typing import FrozenSet
 
 @dataclass(frozen=True)
 class Material():
-    displayname: str
+    display_name: str
     id: str
 
 
 @dataclass(frozen=True)
 class Operating_unit():
-    displayname: str
+    display_name: str
     inputs: FrozenSet[Material]
     outputs: FrozenSet[Material]
 
@@ -30,18 +30,18 @@ class P_graph:
     def _init_from_data_(self, data: dict) -> None:
         self.materials = {id: Material(
             id=id,
-            displayname=m["displayname"]
+            display_name=m["display_name"]
         ) for id, m in data["materials"].items()}
         self.operating_units = [Operating_unit(
             inputs={self.materials[m] for m in o["inputs"]},
             outputs={self.materials[m] for m in o["outputs"]},
-            displayname=o["displayname"]
+            display_name=o["display_name"]
         ) for o in data["operating units"]] # should be set
 
     def __str__(self) -> str:
         return f"""
-            M = {{ {", ".join(m.displayname for m in self.materials.values())} }}
-            O = {{ {", ".join(o.displayname for o in self.operating_units)} }}
+            M = {{ {", ".join(m.display_name for m in self.materials.values())} }}
+            O = {{ {", ".join(o.display_name for o in self.operating_units)} }}
         """
 
 
